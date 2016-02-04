5e528ea2cdcc6e1416404cf3d4aa64f411635053
jmagnuss <jmagnuss@gmail.com>
2016-02-03 16:07:17 -0800

    JM: old changes, but looks like adding code to have front webinstances run the event-api project

---
+++ front/attributes/default.rb
@@ -0,0 +1,2 @@
+default['composer']['install_dir'] = "/usr/local/bin"
+default['composer']['user'] = "root"
--- front/recipes/default.rb
+++ front/recipes/default.rb
@@ -7,3 +7,36 @@
 # All rights reserved - Do Not Redistribute
 #
 
+# Author:: Jeremy Quinton (<jeremyquinton at gmail.com>)
+# Cookbook Name:: Composer
+# from https://github.com/jeremyquinton/composer/blob/master/recipes/default.rb
+script "install_composer" do
+  interpreter "bash"
+  user "#{node['composer']['user']}"
+  cwd "/tmp"
+  code <<-EOH
+  curl -s https://getcomposer.org/installer | php -- --install-dir="#{node['composer']['install_dir']}"
+  EOH
+end
+
+include_recipe "apache2"
+
+apache_site "default" do
+  enable true
+end
+
+deploy 'mainevent-front' do
+  repo 'git@github.com:maineventio/mainevent.git'
+  user 'ubuntu'
+  deploy_to '/var/www/mainevent'
+  action :deploy
+end
+
+template "/etc/httpd/conf.d/mainevent.conf" do
+  source "logstash-sqs.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :restart,"service[logstash]", :delayed
+end
+
