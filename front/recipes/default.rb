f7269cbc997c0e3c6a54db33123880e03cd97895
jmagnuss <jmagnuss@gmail.com>
2016-04-18 06:45:18 -0700

    Trying simpler approach to apache and php

--- front/recipes/default.rb
+++ front/recipes/default.rb
@@ -32,26 +32,26 @@ end
 #include_recipe 'front::apache'
 node.default['apache']['version'] = '2.4'
 node.default['apache']['ext_status'] = true
-include_recipe 'apache2'
+#include_recipe 'apache2'
 #include_recipe 'apache2::mod_ssl'
 # This one looks to be an OpsWorks standard
-include_recipt 'mod_php5_apache2' 
-include_recipe 'apache2::mod_php5'
+#include_recipt 'mod_php5_apache2' 
+#include_recipe 'apache2::mod_php5'
 
-#case node['platform']
-#when 'amazon'
-#  apache_module 'php5' do
-#    filename 'libphp-5.6.so'
-#    conf true
-#    enable true
-#  end
-#when 'centos'
-#  apache_module 'php5' do
-#    filename 'libphp5.so'
-#    conf true
-#    enable true
-#  end
-#end
+case node['platform']
+when 'amazon'
+  apache_module 'php5' do
+    filename 'libphp-5.6.so'
+    conf true
+    enable true
+  end
+when 'centos'
+  apache_module 'php5' do
+    filename 'libphp5.so'
+    conf true
+    enable true
+  end
+end
 
 
 #apache_site "default" do
